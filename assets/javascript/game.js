bb5f5bae080295ee05d673b078e7b3961d0b62fc
Jeff McMahan <mcmahan.jeff@gmail.com>
2017-02-17 10:07:06 -0600

    updated game.js image height and width, updated index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
 window.onload = function(){
 
 	var playerTotal = 0;
+	var wins = 0;
+	var losses = 0;
+	
 	// generats random number between 19 & 120 for #randomStart 
 	var randomStart = Math.floor((Math.random() * 101) +19);
 	console.log(randomStart);
@@ -18,18 +21,29 @@ window.onload = function(){
 		var imageCrystal = $("<img>");
 		imageCrystal.addClass("crystal-image");
 		imageCrystal.attr("src", "assets/image/crystal" + i + ".png");
+		imageCrystal.attr("width", 100);
+		imageCrystal.attr("height",100);
 		imageCrystal.attr("data-crystalValue", crystalArray[i]);
 		$("#crystals").append(imageCrystal);
 
 	}
-
+	// on click takes random value of crystal and adds to player total
+	// and displays on screen
+	// debugger;
 	$(".crystal-image").on("click", function(){
 		var clickedCrystalValue = ($(this).attr("data-crystalValue"));
 		clickedCrystalValue = parseInt(clickedCrystalValue);
 		playerTotal += clickedCrystalValue;
 		console.log(playerTotal);
 		console.log(clickedCrystalValue)
-		$("#playerTotal").html("Current Total: " + playerTotal);
+		$("#playerTotal").html(playerTotal);
+		console.log(randomStart);
+		// add jquery to push wins losses to DOM
+		if (playerTotal === randomStart){
+			wins++;
+		}else if (playerTotal > randomStart){
+			losses++;
+		}
 
 		
 
